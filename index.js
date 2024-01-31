b227087e3743441e45cc3bdf5bf370c501fa96e8
Sylviachelimo <chelimosylvia280@gmail.com>
2024-01-31 13:16:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+function countDown(startingNumber) {
+    let currentNumber = startingNumber;
+  
+    while (currentNumber >= 0) {
+      console.log(currentNumber);
+      currentNumber--;
+    }
+  }
+  
+  countDown(10);
+  
